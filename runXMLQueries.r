123f9867f099209a12223cabe829a45aa15a3ba5
Robert M Flight <r.flight79@gmail.com>
2012-10-25 15:08:35 -0400

    added documentation on the functions

--- runXMLQueries.r
+++ runXMLQueries.r
@@ -9,6 +9,11 @@ testNone <- data.frame(uid=1, qRad=248, qString="46.10416,-64.81636")
 testSome <- data.frame(uid=1, qRad=1000, qString="44.640811,-63.574705")
 testMulti <- data.frame(uid=1, qRad=5000, qString="44.640811,-63.574705")
 
+
+# takes an querystring, parses it, and returns the latitudes and longitudes 
+# inSearch: the query string to use
+# currCount: a counter for the number of queries performed in the alloted time
+# allCount: the total number of queries that have been performed
 searchLoc <- function(inSearch, currCount=0, allCount=0){
 
 	start.time <- now()
@@ -45,6 +50,12 @@ searchLoc <- function(inSearch, currCount=0, allCount=0){
 	return(list(outLoc=outLoc, count=currCount, allCount=allCount, isParse=parsedDat))
 }
 
+# checks if you have gone over the number of queries in the allotted time, and if so, forces a sleep until the time
+# is up
+# startTime: when did you start your calculations
+# maxTime: how much time allowed for the set number of queries
+# currCount: which query are you on
+# maxCount: how many queries allowed in the allotted time
 checkTime <- function(startTime, maxTime, currCount, maxCount){
 	currDiff <- difftime(now(), startTime, units="secs")
 	
@@ -60,6 +71,13 @@ checkTime <- function(startTime, maxTime, currCount, maxCount){
 }
 
 # runBlock: query an entire block of data
+# This takes which blocks you want to work on
+# blockIndex: which blocks of data to process
+# keyFile: the file that has your google places api key
+# inFile: what file has the tab delimited data to process
+# waitTime: how long to wait before processing the next set of data
+# maxEntryTime: how many entries can be processed within "waitTime"
+# maxQueryAll: how many total queries you want to perform
 runBlock <- function(blockIndex, keyFile="googlemapsapi.key", inFile="censusDisseminationLocData.txt", waitTime=24*60*60, maxEntryTime=1000, maxQueryAll=Inf){
 	stopifnot(is.numeric(blockIndex), file.exists(keyFile), file.exists(inFile), is.numeric(waitTime), is.numeric(maxEntryTime))
 	locData <- read.table(inFile, sep="\t", header=T, stringsAsFactors=F)
