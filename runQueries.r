21c2a444f10c5cb91c9d8016a6f96f063d3d0631
rmflight <r.flight79@gmail.com>
2012-11-23 10:52:18 -0500

    added a bunch more timmys

--- runQueries.r
+++ runQueries.r
@@ -1,5 +1,5 @@
 # Actually run some queries
 
 source("runXMLQueries.r")
-runQueries(seq(1, 10000), idFile="clientid.txt", secretFile="clientsecret.txt", inFile="censusDisseminationLocData.txt", outFile="timmysLocs.txt", waitTime=60*60, maxEntryTime=5000, checkTime=100)
+runQueries(seq(8000, 14000), idFile="clientid.txt", secretFile="clientsecret.txt", inFile="censusDisseminationLocData.txt", outFile="timmysLocs.txt", waitTime=60*60, maxEntryTime=5000, checkTime=100)
 cleanUpResults()
