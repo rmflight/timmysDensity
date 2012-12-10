0c1840e572c81c5b5053149bfa32a9f1cd896644
rmflight <r.flight79@gmail.com>
2012-12-10 05:50:28 -0500

    finished running the queries

--- runQueries.r
+++ runQueries.r
@@ -1,5 +1,5 @@
 # Actually run some queries
 
 source("runXMLQueries.r")
-runQueries(seq(8000, 14000), idFile="clientid.txt", secretFile="clientsecret.txt", inFile="censusDisseminationLocData.txt", outFile="timmysLocs.txt", waitTime=60*60, maxEntryTime=5000, checkTime=100)
+runQueries(seq(40001, 52531), idFile="clientid.txt", secretFile="clientsecret.txt", inFile="censusDisseminationLocData.txt", outFile="timmysLocs.txt", waitTime=60*60, maxEntryTime=5000, checkTime=100)
 cleanUpResults()
