b07f92a3038c3adaa3533f2eb61ddb7221dbcb6c
rmflight <r.flight79@gmail.com>
2013-05-31 22:54:23 -0400

    modifying function to work with data frame instead of text file, and removing testing code

--- R/runXMLQueries.r
+++ R/runXMLQueries.r
@@ -1,17 +1,7 @@
-require(RCurl)
-require(RJSONIO)
-require(lubridate)
-
-apiStr <- "https://api.foursquare.com/v2/venues/search?"
-srchStr <- "&query=tim%20hortons&limit=50"
-
-testNone <- data.frame(uid=1, qRad=248, qString="46.10416,-64.81636")
-testSome <- data.frame(uid=1, qRad=1000, qString="44.640811,-63.574705")
-testMulti <- data.frame(uid=1, qRad=5000, qString="44.640811,-63.574705")
-
-
 #' takes an querystring, parses it, and returns the latitudes and longitudes 
 #' @param inSearch the query string to use
+#' @importFrom RCurl getURL
+#' @importFrom RJSONIO fromJSON
 searchLoc <- function(inSearch){
   outLoc <- "" #default to return
 	start.time <- now()
@@ -41,6 +31,7 @@ searchLoc <- function(inSearch){
 #'   \item{sTime: }{the start time of the queries}
 #'   \item{count: }{the current count}
 #' }
+#' @importFrom lubridate now
 checkTime <- function(startTime, maxTime, currCount, maxCount){
 	currDiff <- difftime(now(), startTime, units="secs")
 	
@@ -56,32 +47,41 @@ checkTime <- function(startTime, maxTime, currCount, maxCount){
 }
 
 
-#' query a bunch of data
-#' @param blockIndex which blocks of data to process
-#' @param keyFile the file that has your google places api key
-#' @param inFile what file has the tab delimited data to process
+#' run foursquare api queries
+#' @param queryIndex which entries of data to process
+#' @param idFile the file that has your foursquare api key
+#' @param secretFile file with the foursquare clientsecret
+#' @param inData the data to process
+#' @param outFile where to save the results of the search
 #' @param waitTime how long to wait before processing the next set of data
 #' @param maxEntryTime how many entries can be processed within "waitTime"
 #' @param maxQueryAll how many total queries you want to perform
-runQueries <- function(queryIndex, idFile="clientid.txt", secretFile="clientsecret.txt", inFile="censusDisseminationLocData.txt", outFile="timmysLocs.txt", waitTime=60*60, maxEntryTime=5000, checkTime=100){
+#' @export
+#' @details expects \code{inData} to be a data frame with \code{lat}, \code{long}, and \code{block} numeric columns. \code{lat} and \code{long} are used as the locations to query foursquare, \code{block} controls
+#' @importFrom lubridate now
+runQueries <- function(inData, idFile="clientid.txt", secretFile="clientsecret.txt", outFile="timmysLocs.txt", waitTime=60*60, maxEntryTime=5000, checkTime=100){
 	stopifnot(is.numeric(queryIndex), file.exists(idFile), file.exists(secretFile), file.exists(inFile), is.numeric(waitTime), is.numeric(maxEntryTime))
-	locData <- read.table(inFile, sep="\t", header=T, stringsAsFactors=F)
 	
-	useid <- scan(idFile, "character")
+  # foursquare api information
+	apiStr <- "https://api.foursquare.com/v2/venues/search?"
+	srchStr <- "&query=tim%20hortons&limit=50"
+  
+  useid <- scan(idFile, "character")
   useSecret <- scan(secretFile, "character")
   clientStr <- paste("&client_id=", useid, "&client_secret=", useSecret, "&v=", as.character(now(), format="%Y%m%d"), sep="", collapse="")
 	
-	
-	nQuery <- length(queryIndex)
+	inData$qString <- paste(inData$lat, inData$long, sep=",")
+  
+	nQuery <- nrow(inData)
 	getCount <- 0 # keeps track of how many files we have downloaded, ie how many queries did we make on the server
 	allCount <- 0
-	startTime <- now() # when are we starting, want to know because we can only make 1000 queries in 24 hours
+	startTime <- now() # when are we starting, want to know because we can only make so many queries in a particular time period
 	
   useSplit <- seq(1, length(queryIndex) / checkTime)
   splitIndx <- split(queryIndex, rep(useSplit, length(queryIndex) / length(useSplit)))
   
   sapply(splitIndx, function(inIndx){
-    sapply(locData$qString[inIndx], function(useLoc){
+    sapply(inData$qString[inIndx], function(useLoc){
       locStr <- paste("ll=", useLoc, sep="", collapse="")
       qStr <- paste(apiStr, locStr, srchStr, clientStr, sep="", collapse="")
       resultLoc <- searchLoc(qStr)
@@ -101,45 +101,10 @@ runQueries <- function(queryIndex, idFile="clientid.txt", secretFile="clientsecr
 #' cleanup the results to make sure only unique results
 #' @param inLocFile the file with the locations
 #' @details takes a file containing the Tims locations, and makes sure that only unique entries are in the file. As a side effect, simply writes the data back to the same file.
+#' @export
 cleanUpResults <- function(inLocFile="timmysLocs.txt"){
   stopifnot(file.exists(inLocFile))
   allLocs <- scan(inLocFile, what=character(), sep="\n")
   allLocs <- unique(allLocs)
   cat(allLocs, file=inLocFile, sep="\n", append=F)
-}
-
-#' run some simple tests
-runTests <- function(){
-  useid <- scan("clientid.txt", "character")
-  useSecret <- scan("clientsecret.txt", "character")
-  clientStr <- paste("&client_id=", useid, "&client_secret=", useSecret, "&v=", as.character(now(), format="%Y%m%d"), sep="", collapse="")
-	inLoc <- testNone
-	locStr <- paste("ll=", inLoc$qString, sep="", collapse="")
-	qStr <- paste(apiStr, locStr, srchStr, clientStr, sep="", collapse="")
-	resultLocNone <- searchLoc(qStr)
-	
-	inLoc <- testSome
-  clientStr <- paste("&client_id=", useid, "&client_secret=", useSecret, "&v=", as.character(now(), format="%Y%m%d"), sep="", collapse="")
-  locStr <- paste("ll=", inLoc$qString, sep="", collapse="")
-  qStr <- paste(apiStr, locStr, srchStr, clientStr, sep="", collapse="")
-	resultLocSome <- searchLoc(qStr)
-	
-	inLoc <- testMulti
-  clientStr <- paste("&client_id=", useid, "&client_secret=", useSecret, "&v=", as.character(now(), format="%Y%m%d"), sep="", collapse="")
-  locStr <- paste("ll=", inLoc$qString, sep="", collapse="")
-  qStr <- paste(apiStr, locStr, srchStr, clientStr, sep="", collapse="")
-	resultLocMulti <- searchLoc(qStr, 0)
-}
-
-
-runQueryTest <- function(){
-  runQueries(seq(1,100), idFile="clientid.txt", secretFile="clientsecret.txt", inFile="censusDisseminationLocData.txt", outFile="timmysLocs.txt", waitTime=60*60, maxEntryTime=5000, checkTime=100)
-}
-
-runTimeTest <- function(){
-  t1 <- now()
-  runQueries(seq(1,300), idFile="clientid.txt", secretFile="clientsecret.txt", inFile="censusDisseminationLocData.txt", outFile="timmysLocs.txt", waitTime=60*60, maxEntryTime=5000, checkTime=100)
-  t2 <- now()
-  tDiff <- difftime(t2,t1,units="s")
-  tDiff / 300
 }
